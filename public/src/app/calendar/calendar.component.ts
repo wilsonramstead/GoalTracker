0580e6652393aed08dad3331b12c7b35a8c88e95
Wilson Ramstead <ramsteadwilson@gmail.com>
2019-10-24 13:19:23 -0700

    calendar info + styling + animations updated, backend logic simplified and cleaned up

--- public/src/app/calendar/calendar.component.ts
+++ public/src/app/calendar/calendar.component.ts
@@ -11,18 +11,17 @@ export class CalendarComponent implements OnInit {
   @Output() navLink = new EventEmitter<string>();
 
   constructor(private _httpService: HttpService) { }
-
   weeks: any;
   weekDays: any;
   monthNum: any;
-  // monthNums: any;
-  currentMonth: any
+  monthString: any;
+  dayOfWeek: any;
+  dayOfMonth: any;
   count: any;
   firstGoal: any;
   selectMessage: any;
   monthDays: any;
   currentGoal: any;
-  currentDay: any;
 
   ngOnInit() {
     $(document).ready(function() { 
@@ -33,61 +32,35 @@ export class CalendarComponent implements OnInit {
       })
     })
     this.weeks = [1,2,3,4]
-    // this.monthNums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+    this.monthNum = 0;
     this.weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-
-
-
-    this.currentMonth = this.getMonth();
     this.count = 2;
-    this.currentDay = this.getCurrentDay();
     this.getGoals();
+    this.getDateInfo();
     // this.firstGoal = this.allGoals[0];
     this.selectMessage = 'Select a Goal';
     this.monthDays = [];
-    console.log('this.monthDays: ', this.monthDays);
   }
 
-  getCurrentDay() {
-    const date = new Date();
-    const day = date.getDate();
-    console.log('day: ', day);
-    return day;
-  }
-
-  dayNums() {
-    if(this.count >= 31) {
-      this.count = 0;
-    } else {
-      this.count++;      
-    }
-    return this.count;
-  }
-
-  getMonth() {
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const date = new Date();
-    console.log('date: ', date);
-    const month = monthNames[date.getMonth()];
-    this.monthNum = date.getMonth()+1;
-    console.log('month: ', month);
-    return month;
-  }
-
-
-
-  goalSelect(id) {
-    console.log('id: ', id);
-    let observable = this._httpService.findOne(id);
-    observable.subscribe(data => {
-      this.currentGoal = data['data'];
-      this.selectMessage = data['data'].Name;
-      this.monthDays = data['data']['CurrentMonth'];
-      console.log('this.monthDays: ', this.monthDays);
-      console.log('data: ', data);
+  animation() {
+    console.log('fukin genius');
+    $(document).ready(function() {
+      $('.day').hover(function() {
+        $(this).css('transform', 'scale(1.1)');
+        $(this).css('background', 'radial-gradient(white, rgb(255, 249, 167))');
+      }, function() {
+        $(this).css('transform', 'scale(1.0)');
+        $(this).css('background', 'radial-gradient(white, silver)');
+      })
     })
-    return this.monthDays;
+  }
+
+  applyHoverAnimation() {
+    setTimeout(this.animation, 50);
+  }
+
+  callParent(string) {
+    this.navLink.next(string);
   }
 
   allGoals = [];
@@ -100,7 +73,35 @@ export class CalendarComponent implements OnInit {
     return this.allGoals;
   }
 
-  callParent(string) {
-    this.navLink.next(string);
+  goalSelect(id) {
+    let observable = this._httpService.findOne(id);
+    observable.subscribe(data => {
+      this.currentGoal = data['data'];
+      this.selectMessage = data['data'].Name;
+      this.monthDays = data['data']['CurrentMonth'];
+    })
+    return this.monthDays;
+  }
+  
+  getDateInfo() {
+    const date = new Date();
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.monthString = monthNames[date.getMonth()];
+    console.log('monthString: ', this.monthString)
+    this.monthNum = date.getMonth()+1;
+    console.log('monthNum: ', this.monthNum);
+    this.dayOfMonth = date.getDate();
+    console.log('dayOfMonth: ', this.dayOfMonth);
+    this.dayOfWeek = date.getDay();
+    console.log('dayOfWeek: ', this.dayOfWeek);
+  }
+
+  dayNums() {
+    if(this.count >= 31) {
+      this.count = 0;
+    } else {
+      this.count++;      
+    }
+    return this.count;
   }
 }
--- public/src/app/new-goal/new-goal.component.ts
+++ public/src/app/new-goal/new-goal.component.ts
@@ -14,27 +14,25 @@ export class NewGoalComponent implements OnInit {
   constructor(private _httpService: HttpService) { }
 
   newGoal: any;
+  monthString: any;
   monthNum: any;
-  currentMonth: any;
   allGoals: any;
   weekDays: any;
-  date: any;
   dayOfMonth: any;
   dayOfWeek: any;
 
   ngOnInit() {
     this.getGoals();
+    this.getDateInfo();
     this.newGoal = { 'Name': '', 'Description': '', 'CurrentMonthName': '', 'CurrentMonth': [{'Day': 1, 'WeekDay': '', 'Status': 'undefined'}, {'Day': 2, 'WeekDay': '', 'Status': 'undefined'}, {'Day': 3, 'WeekDay': '', 'Status': 'undefined'}, {'Day': 4, 'WeekDay': '', 'Status': 'undefined'}, {'Day': 5, 'WeekDay': '', 'Status': 'undefined'}, {'Day': 6, 'WeekDay': '', 'Status': 'undefined'}, {'Day': 7, 'WeekDay': '', 'Status': 'undefined'}, {'Day': 8, 'WeekDay': '', 'Status': 'undefined'}, {'Day': 9, 'WeekDay': '', 'Status': 'undefined'},{'Day': 10, 'WeekDay': '', 'Status': 'undefined'},{'Day': 11, 'WeekDay': '', 'Status': 'undefined'},{'Day': 12, 'WeekDay': '', 'Status': 'undefined'},{'Day': 13, 'WeekDay': '', 'Status': 'undefined'},{'Day': 14, 'WeekDay': '', 'Status': 'undefined'},{'Day': 15, 'WeekDay': '', 'Status': 'undefined'},{'Day': 16, 'WeekDay': '', 'Status': 'undefined'},{'Day': 17, 'WeekDay': '', 'Status': 'undefined'},{'Day': 18, 'WeekDay': '', 'Status': 'undefined'},{'Day': 19, 'WeekDay': '', 'Status': 'undefined'},{'Day': 20, 'WeekDay': '', 'Status': 'undefined'},{'Day': 21, 'WeekDay': '', 'Status': 'undefined'},{'Day': 22, 'WeekDay': '', 'Status': 'undefined'},{'Day': 23, 'WeekDay': '', 'Status': 'undefined'},{'Day': 24, 'WeekDay': '', 'Status': 'undefined'},{'Day': 25, 'WeekDay': '', 'Status': 'undefined'},{'Day': 26, 'WeekDay': '', 'Status': 'undefined'},{'Day': 27, 'WeekDay': '', 'Status': 'undefined'},{'Day': 28, 'WeekDay': '', 'Status': 'undefined'},{'Day': 29, 'WeekDay': '', 'Status': 'undefined'},{'Day': 30, 'WeekDay': '', 'Status': 'undefined'},{'Day': 31, 'WeekDay': '', 'Status': 'undefined'}]};
     this.weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];    
-    this.getMonth();
-    this.getCurrentDay();
   }
   callParent(string) {
     this.navLink.next(string);
   }
 
   createGoal() {
-    this.newGoal['CurrentMonthName'] = this.getMonth();
+    this.newGoal['CurrentMonthName'] = this.monthString;
     for(var i = 0; i <= this.dayOfWeek-1; i++) {
       this.weekDays.push(this.weekDays.shift());
     }
@@ -58,15 +56,6 @@ export class NewGoalComponent implements OnInit {
     })
   }
   
-  getCurrentDay() {
-    this.date = new Date();
-    this.dayOfMonth = this.date.getDate();
-    this.dayOfWeek = this.date.getDay();
-    console.log('day: ', this.dayOfMonth);
-    console.log('dayOfWeek: ', this.dayOfWeek);
-    return this.dayOfMonth;
-  }
-
   getGoals() {
     let observable = this._httpService.getGoals();
     observable.subscribe(data => {
@@ -74,11 +63,16 @@ export class NewGoalComponent implements OnInit {
     })
   }
 
-  getMonth() {
-    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  getDateInfo() {
     const date = new Date();
-    this.currentMonth = monthNames[date.getMonth()];
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.monthString = monthNames[date.getMonth()];
+    console.log('monthString: ', this.monthString)
     this.monthNum = date.getMonth()+1;
-    return this.currentMonth;
+    console.log('monthNum: ', this.monthNum);
+    this.dayOfMonth = date.getDate();
+    console.log('dayOfMonth: ', this.dayOfMonth);
+    this.dayOfWeek = date.getDay();
+    console.log('dayOfWeek: ', this.dayOfWeek);
   }
 }
